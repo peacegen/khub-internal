5e9992432b0fd60245f894ef69c896184a6a97a5
pricejoshua <joshua.j.price@my.wheaton.edu>
2022-08-22 12:46:39 +0700

    Worked on frontpage looks

--- resources/views/livewire/frontpage.blade.php
+++ resources/views/livewire/frontpage.blade.php
@@ -1,42 +1,8 @@
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-        @trixassets
-
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body>
-        <div class="min-h-screen bg-gray-100">
-            {{-- //TODO Remove this and have a better top menu --}}
-            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                @if (Route::has('login'))
-                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                        @auth
-                            <a href="{{ url('/pages') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">{{ __('Pages') }}</a>
-                        @else
-                            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">{{ __('Log in') }}</a>
-
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">{{ __('Register') }}</a>
-                            @endif
-                        @endauth
-                    </div>
-                @endif
-            </div>
-            <h1>{{ $title }}</h1>
-            {!! $content->render() !!}
-        </div>
-    </body>
-</html>
+<div>
+    <nav class="bg-gray-900 px-3 py-2 flex items-center">
+        <div>
+            <button class="block h-8 mr-3 text-gray-400 items-center">
+                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                </svg>
+            </button>
