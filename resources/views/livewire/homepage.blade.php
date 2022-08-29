28b51b75438a5034436a67d708df148b93cc0fd1
pricejoshua <joshua.j.price@my.wheaton.edu>
2022-08-29 15:36:57 +0700

    Worked on some styling

--- resources/views/livewire/homepage.blade.php
+++ resources/views/livewire/homepage.blade.php
@@ -1,11 +1,11 @@
-<div>
+<div class="flex justify-center flex-col items-center mt-4">
     {{-- // TODO Work more on the homepage --}}
     @foreach ($data as $tag)
-    <div>
-        <div>{{ $tag['name'] }}</div>
-        <div>{{ $tag['description'] }}</div>
-        @include('components.carousel', ['items' => $tag['items']])
-    </div>
+        <div class="w-3/4 origin-center mt-4">
+            <div class="mt-4 text-lg">{{ $tag['name'] }}</div>
+            <div class="mt-4 text-sm">{{ $tag['description'] }}</div>
+            @include('components.carousel', ['items' => $tag['items']])
+        </div>
     @endforeach
 
     <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
