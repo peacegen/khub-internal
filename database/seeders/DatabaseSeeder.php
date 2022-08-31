cf8c238a87d55da66272780e01389676a8cc04bd
pricejoshua <joshua.j.price@my.wheaton.edu>
2022-08-31 14:30:21 +0700

    Added admin to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \App\Models\User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin'),
+            'role' => 'admin',
+        ]);
         \App\Models\User::factory(10)->create();
         \App\Models\Page::factory(10)->create();
     }
