247a867bda1cccf851f3ae00a3bc86e77df710bb
pricejoshua <joshua.j.price@my.wheaton.edu>
2022-08-31 13:51:38 +0700

    Small changes

--- resources/views/livewire/homepage.blade.php
+++ resources/views/livewire/homepage.blade.php
@@ -1,6 +1,6 @@
 <div class="flex justify-center flex-col items-center mt-4">
     {{-- // TODO Work more on the homepage --}}
-    @if (count($data))
+    @if ($data && count($data))
     @foreach ($data as $tag)
         <div class="w-3/4 origin-center mt-4">
             <div class="mt-4 text-lg">{{ $tag['name'] }}</div>
@@ -9,6 +9,9 @@
         </div>
     @endforeach
     @endif
+    <div class="text-lg">
+        Nothing to see here.
+    </div>
 
     <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
 
