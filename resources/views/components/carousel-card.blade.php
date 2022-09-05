60dfd46a02015a7eae604fc243e2be9901c11bf0
pricejoshua <joshua.j.price@my.wheaton.edu>
2022-09-05 14:45:55 +0700

    removed a semicolon

--- resources/views/components/carousel-card.blade.php
+++ resources/views/components/carousel-card.blade.php
@@ -1,12 +1,10 @@
-
-  <div class="relative">
+<div class="relative">
     <a href="{{ $link }}" class="absolute inset-0 z-10 bg-white bg-opacity-0 text-center flex flex-col items-center justify-center hover:bg-opacity-70 duration-300">
         <div class=tracking-wider > {{ $content }} </div>
     </a>
-    <a href="{{ $link }}" class="relative">;
+    <a href="{{ $link }}" class="relative">
         <div class="flex flex-wrap content-center">
             <img src="{{ $backgroundUrl }}" class="mx-auto" alt="">
         </div>
     </a>
-
-  </div>
+</div>
