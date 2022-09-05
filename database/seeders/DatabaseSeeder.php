de34354430617031f425e3d020727907665e3c3a
pricejoshua <joshua.j.price@my.wheaton.edu>
2022-09-05 09:46:21 +0700

    Adding permissons

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,22 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $permisson = \App\Models\Permission::create([
-            'name' => 'Manage Pages',
-            'description' => 'Can manage pages',
-            'page' => 'pages',
-        ]);
-        $admin = \App\Models\Role::create([
-                'name' => 'admin',
-                'description' => 'Administrator',
-        ]);
-        $admin->permissions()->attach($permisson);
         $user = \App\Models\User::create([
             'name' => 'Admin',
             'email' => 'admin@admin.com',
             'password' => bcrypt('password'),
         ]);
-        $user->roles()->attach($admin);
         \App\Models\User::factory(10)->create();
         \App\Models\Page::factory(10)->create();
         \App\Models\Tag::factory(3)->create();
