1f1278ece9e5387840ee2fb6322807b8967bb3ee
pricejoshua <joshua.j.price@my.wheaton.edu>
2022-09-22 16:37:32 +0700

    Worked on new page page

--- app/Http/Livewire/EditPage.php
+++ app/Http/Livewire/EditPage.php
@@ -3,19 +3,49 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Page;
+use App\Models\Tag;
 
 class EditPage extends Component
 {
     public $urlslug;
     public $page;
 
+    public $title;
+    public $content;
+    public $slug;
+    public $tags = [];
+
     public function mount($urlslug = null)
     {
         $this->urlslug = $urlslug;
     }
 
+    public function loadModel($slug)
+    {
+        $model = Page::where('slug', $slug)->first();
+        $this->page = $model;
+        $this->title = $model->title;
+        $this->slug = $model->slug;
+        $this->content = $model->content;
+        $this->tags = $model->tags;
+    }
+
+    /**
+     * Loads all Tags in a list
+     */
+    public function tag_list(){
+        return Tag::all()->pluck('name')->toArray();
+    }
+    
+    public function cancel(){
+        return redirect()->route('pages.index');
+    }
+
+
     public function render()
     {
+        $this->loadModel($this->urlslug);
         return view('livewire.edit-page')->layout('layouts.frontpage');
     }
 }
--- resources/views/livewire/edit-page.blade.php
+++ resources/views/livewire/edit-page.blade.php
@@ -1,3 +1,54 @@
-<div class="mx-auto">
-    <x-page-form :urlslug="$urlslug"/>
+<div>
+    <x-jet-form-section submit="createPage">
+        <x-slot name="title">
+            {{ __('Save Page') }}
+        </x-slot>
+
+        <x-slot name="description">
+            {{ __('Create a new page.') }}
+        </x-slot>
+
+
+        <x-slot name="form">
+            <div class="col-span-6">
+                <x-jet-label for="title" value="{{ __('Page Title') }}" />
+                <x-jet-input id="title" class="block mt-1 w-full" type="text" wire:model.debounce.800ms="title" required />
+                @error('title') <span class="error">{{ $message }}</span> @enderror
+            </div>
+            <div class="col-span-6">
+                <x-jet-label for="slug" value="{{ __('Slug') }}" />
+                <x-jet-input id="slug" class="block mt-1 w-full" type="text" wire:model.debounce.800ms="slug" required />
+                @error('slug') <span class="error">{{ $message }}</span> @enderror
+            </div>
+            <div>
+            @isset($tag_list)
+                <div wire:ignore>
+                    <select data-pharaonic="select2" multiple data-component-id="{{ $this->id }}" wire:model="tags">
+                        @foreach ($tag_list() as $tag)
+                            <option value="{{ $tag }}">{{ $tag }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            @endisset
+            </div>
+        </x-slot>
+
+        <x-slot name="actions">
+            <x-jet-secondary-button wire:click="cancel" wire:loading.attr="disabled">
+                {{ __('Cancel') }}
+            </x-jet-secondary-button>
+            @isset ($page->id)
+                <x-jet-button class="ml-2" wire:click="update" wire:loading.attr="disabled">
+                    {{ __('Update') }}
+                </x-jet-button>
+            @else
+                <x-jet-button class="ml-2" wire:click="create" wire:loading.attr="disabled">
+                    {{ __('Create') }}
+                </x-jet-button>
+            @endisset
+        </x-slot>
+
+
+    </x-jet-form-section>
 </div>
+
