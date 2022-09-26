741e0400c08dc1abb0fc778ae19cfe63777d8f6d
pricejoshua <joshua.j.price@my.wheaton.edu>
2022-09-26 14:32:52 +0700

    Got some config set up

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Tonysm\RichTextLaravel\Models\Traits\HasRichText;
+use Spatie\MediaLibrary\HasMedia;
 
 class Page extends Model
 {
     use HasFactory;
     use HasRichText;
+    use HasMedia;
 
     protected $guarded = [];
 
