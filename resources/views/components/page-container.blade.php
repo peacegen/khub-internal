4b98d1ac950146340920e01614099c342e765ab8
pricejoshua <joshua.j.price@my.wheaton.edu>
2022-09-28 16:56:32 +0700

    Prettied it up a bit

--- resources/views/components/page-container.blade.php
+++ resources/views/components/page-container.blade.php
@@ -1,9 +1,9 @@
 <div>
 <a href="{{ URL::to('/pages/'.$page->slug) }}" class="relative">
-<div class="py-2 flex">
+<div class="py-4 flex">
     <img src="{{ $page->thumbnail_url }}" class="w-1/3" alt="{{ $page->title }}"/>
-    <div>
-        <div class="text-lg py-2 px-2 truncate">
+    <div class="w-2/3">
+        <div class="text-lg px-2">
         {{ $page->title }}
         </div>
         <div class="text-sm py-2 px-2 truncate">
