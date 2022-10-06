6456123432dc9027b252ba25e6343933cde555ba
pricejoshua <joshua.j.price@my.wheaton.edu>
2022-10-06 15:47:02 +0700

    Continued working on styling

--- resources/views/components/tag-container.blade.php
+++ resources/views/components/tag-container.blade.php
@@ -1,5 +1,3 @@
-<div class="text-xs inline-flex items-center leading-sm px-3 py-1 bg-primary-background rounded-full">
-    <a href="{{ URL::to('/tags/'.$tag->slug) }}" class="text-primary-text">
-        {{ $tag->name }}
-    </a>
-</div>
+<a href="{{ URL::to('/tags/'.$tag->slug) }}" class="text-xs inline-flex items-center leading-sm px-3 py-1 bg-primary-100 outline outline-primary-500 outline-1 rounded-full text-primary-500">
+    {{ $tag->name }}
+</a>
