36eca173e9cb4170cf36c495bfc6f2446e7fecb3
pricejoshua <joshua.j.price@my.wheaton.edu>
2022-10-26 10:47:16 +0700

    changed custom js stuff

--- resources/views/layouts/frontpage.blade.php
+++ resources/views/layouts/frontpage.blade.php
@@ -19,7 +19,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
-        <script src="{{ asset("vendor/pharaonic/pharaonic.select2.js") }}"></script>
+        <script src="{{ asset("assets/js/pharaonic.select2.js") }}"></script>
 
         <!-- Carousel Style -->
         @yield('carousel-styles')
